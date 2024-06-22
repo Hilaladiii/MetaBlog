3b8b8676ff65324de57cf509f8be9cca73e11649
Hilaladiii <abdulhilal242@gmail.com>
2024-06-22 18:19:58 +0700

    refactor : fix error react hook form using forwardRef

--- src/common/components/elements/Input.tsx
+++ src/common/components/elements/Input.tsx
@@ -1,33 +1,40 @@
 import { cn } from "@/common/lib/utils";
-import { HtmlHTMLAttributes } from "react";
+import { HtmlHTMLAttributes, forwardRef } from "react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 interface InputProps extends HtmlHTMLAttributes<HTMLInputElement> {
-  type: "text" | "number";
+  type: "text" | "number" | "email" | "password";
   placeholder: string;
   size?: "default" | "small" | "large" | "full";
+  name: string;
 }
 
-export default function Input({
-  className,
-  type,
-  size = "default",
-  placeholder,
-  ...props
-}: InputProps) {
-  return (
-    <input
-      type={type}
-      {...props}
-      placeholder={placeholder}
-      className={cn(
-        `bg-secondary100 placeholder:text-secondary400 rounded-md px-4 py-2 ${inter.className}`,
-        {
-          "w-44": size == "default",
-        },
-      )}
-    />
-  );
-}
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, size = "default", placeholder, name, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        className={cn(
+          `rounded-md bg-secondary100 px-4 py-1 placeholder:text-sm placeholder:text-secondary400 ${inter.className}`,
+          {
+            "w-44": size == "default",
+            "w-full": size == "full",
+            "w-24": size == "small",
+            "w-64": size == "large",
+          },
+          className,
+        )}
+        {...props}
+      />
+    );
+  },
+);
+
+Input.displayName = "Input";
+
+export default Input;
