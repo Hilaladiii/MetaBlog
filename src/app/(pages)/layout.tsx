6fe7325dd66443f6efa762242bd5922358003839
Hilaladiii <abdulhilal242@gmail.com>
2024-06-22 18:15:39 +0700

    feat : configure session provider

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
+import { SessionProviders } from "@/common/components/layouts/SessionProvider";
 
 const work_sans = Work_Sans({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={work_sans.className}>{children}</body>
+      <body className={work_sans.className}>
+        <SessionProviders>{children}</SessionProviders>
+      </body>
     </html>
   );
 }
