c12b8c1159b8d9ef98c361f04b2c7463f439cfea
Hilaladiii <abdulhilal242@gmail.com>
2024-06-22 18:27:25 +0700

    feat : middleware for authorization

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+import withAuth from "./middlewares/withAuth";
+
+export async function mainMiddleware(req: NextRequest) {
+  return NextResponse.next();
+}
+
+export default withAuth(mainMiddleware, ["/blog", "/"]);
