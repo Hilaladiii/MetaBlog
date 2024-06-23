ec5beea6c6f5620c8840f80df0934ac0469a1aa1
Hilaladiii <abdulhilal242@gmail.com>
2024-06-23 22:17:02 +0700

    feat : user service signup

---
+++ src/services/user.ts
@@ -0,0 +1,23 @@
+import { UserType } from "@/common/types/user";
+
+export async function signUp(data: UserType) {
+  try {
+    const response = await fetch("/api/auth/sign-up", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const res = await response.json();
+    if (!response.ok) {
+      throw new Error(res.message || "Failed to sign up");
+    }
+
+    return res;
+  } catch (error) {
+    console.error("Sign up error:", error);
+    throw error;
+  }
+}
