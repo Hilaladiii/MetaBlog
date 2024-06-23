b3db74d376a201f90a0879b5ace0ad9bd9cab3c5
Hilaladiii <abdulhilal242@gmail.com>
2024-06-24 05:29:34 +0700

    feat : api route get post by id

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -1,7 +1,14 @@
-import { getPosts } from "@/common/db/blog";
-import { NextResponse } from "next/server";
+import { getPostById, getPosts } from "@/common/db/blog";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  const res = await getPosts();
-  return NextResponse.json(res, { status: res.status });
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get("id");
+  if (id) {
+    const res = await getPostById(id);
+    return NextResponse.json(res, { status: res.status });
+  } else {
+    const res = await getPosts();
+    return NextResponse.json(res, { status: res.status });
+  }
 }
