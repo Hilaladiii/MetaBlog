a3ac5df03c3bf543b1a97a592c53f7b5ee92b8ff
Hilaladiii <abdulhilal242@gmail.com>
2024-06-26 23:29:24 +0700

    chore : implement reusable function get data

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -1,14 +1,14 @@
-import { getPostById, getPosts } from "@/common/db/blog";
+import { getData, getDataById } from "@/common/db/blog";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const id = searchParams.get("id");
   if (id) {
-    const res = await getPostById(id);
+    const res = await getDataById("posts", id);
     return NextResponse.json(res, { status: res.status });
   } else {
-    const res = await getPosts();
+    const res = await getData("posts");
     return NextResponse.json(res, { status: res.status });
   }
 }
