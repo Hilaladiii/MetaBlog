799ace234672d4d83ba4f5b76a7e319e9d7f2f23
Hilaladiii <abdulhilal242@gmail.com>
2024-06-28 10:09:50 +0700

    chore : add color badge component

--- src/common/components/elements/Badge.tsx
+++ src/common/components/elements/Badge.tsx
@@ -1,7 +1,16 @@
+import { cn } from "@/common/lib/utils";
+
 export default function Badge({ title }: { title: string }) {
   return (
-    <span className="rounded-sm bg-sky px-3 py-1 text-xs text-white">
-      {title}
+    <span
+      className={cn("rounded-sm px-3 py-1 text-xs text-white", {
+        "bg-sky": title == "technology",
+        "bg-green-500": title == "education",
+        "bg-red-500": title == "health",
+        "bg-orange-500": title == "economic",
+      })}
+    >
+      {title.substring(0, 1).toUpperCase() + title.substring(1, title.length)}
     </span>
   );
 }
