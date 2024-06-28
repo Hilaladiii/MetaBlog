f9586385170e65e0a6dd01944b63df610219087f
Hilaladiii <abdulhilal242@gmail.com>
2024-06-28 15:45:31 +0700

    feat : add upload blog type & schema

--- src/common/types/blog.ts
+++ src/common/types/blog.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-const MAX_FILE_SIZE = 200000;
+const MAX_FILE_SIZE = 2000000;
 const ACCEPTED_IMAGE_TYPES = [
   "image/jpeg",
   "image/jpg",
@@ -8,10 +8,17 @@ const ACCEPTED_IMAGE_TYPES = [
   "image/webp",
 ];
 
-export const blogSchema = z.object({
+export const uploadBlogSchema = z.object({
   id: z.string().optional(),
   title: z.string().max(255),
-  content: z.string().min(500),
+  content: z.string().min(200),
+  category: z.enum([
+    "technology",
+    "education",
+    "health",
+    "economic",
+    "religion",
+  ]),
   image: z
     .any()
     .refine((files) => files?.length == 1, "Image is required.")
@@ -23,7 +30,14 @@ export const blogSchema = z.object({
       (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
       ".jpg, .jpeg, .png and .webp files are accepted.",
     ),
-  author: z.string(),
 });
+export const blogSchema = z
+  .object({
+    author: z.string(),
+    views: z.number().nonnegative().optional(),
+    createdAt: z.string().optional(),
+  })
+  .merge(uploadBlogSchema);
 
 export type BlogType = z.infer<typeof blogSchema>;
+export type UploadBlogType = z.infer<typeof uploadBlogSchema>;
