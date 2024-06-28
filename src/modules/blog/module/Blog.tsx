e245d8a1a758c130a08b540592c194e81e430061
Hilaladiii <abdulhilal242@gmail.com>
2024-06-28 10:09:28 +0700

    feat : add blog page

--- src/modules/blog/module/Blog.tsx
+++ src/modules/blog/module/Blog.tsx
@@ -1,20 +1,28 @@
 import CardBlog from "@/common/components/fragments/CardBlog";
 import TopBlog from "./TopBlog";
+import { getPosts } from "@/services/blog";
+import { BlogType } from "@/common/types/blog";
 
-export default function Blog() {
+export default async function Blog() {
+  const data = await getPosts();
+  const blogData = data.data as { id: string; data: BlogType }[];
   return (
     <div>
       <div className="w-full">
         <TopBlog />
-        <div className="mx-auto mb-20 mt-52 flex max-w-[68em] flex-col">
-          <div className="grid grid-cols-3 gap-5">
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-            <CardBlog />
-          </div>
+        <div className="mt-20 grid grid-cols-3 gap-5">
+          {blogData &&
+            blogData.map((blog, index) => (
+              <CardBlog
+                title={blog.data.title}
+                author={blog.data.author}
+                image={blog.data.image}
+                date={blog.data.createdAt || ""}
+                category={blog.data.category}
+                href={`/blog/${blog.id}`}
+                key={index}
+              />
+            ))}
         </div>
       </div>
     </div>
