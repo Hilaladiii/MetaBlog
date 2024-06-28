1548e42d3e5629fcd0b18f9b7996d1db495cf89f
Hilaladiii <abdulhilal242@gmail.com>
2024-06-28 10:08:46 +0700

    chore : add props

--- src/common/components/fragments/CardBlog.tsx
+++ src/common/components/fragments/CardBlog.tsx
@@ -1,22 +1,37 @@
 import Image from "next/image";
 import Badge from "../elements/Badge";
 import Author from "./Author";
+import Link from "next/link";
 
-export default function CardBlog() {
+export default function CardBlog({
+  title,
+  image,
+  author,
+  date,
+  category,
+  href,
+}: {
+  title: string;
+  image: string;
+  author: string;
+  date: string;
+  category: string;
+  href: string;
+}) {
   return (
     <div className="max-w-sm rounded-md border-2 border-cloud p-3">
-      <Image
-        src={"/topblog.png"}
-        alt="topblog"
-        width={500}
-        height={500}
-        className="mb-6 h-[250px] w-[380px] rounded-md object-cover object-center"
-      />
-      <Badge title="Technology" />
-      <h1 className="mb-6 mt-4 text-2xl font-semibold">
-        The Impact of Technology on the Workplace: How Technology is Changing
-      </h1>
-      <Author date="August 20, 2022" name="Jason Fransisco" image="" />
+      <Link href={href}>
+        <Image
+          src={image}
+          alt={title}
+          width={500}
+          height={500}
+          className="mb-6 h-[250px] w-[390px] rounded-md object-cover object-center"
+        />
+      </Link>
+      <Badge title={category} />
+      <h1 className="mb-6 mt-4 h-24 text-2xl font-semibold">{title}</h1>
+      <Author date={date} name={author} image="" />
     </div>
   );
 }
