f0564015735f83801b829d9dcdbb5afcab845398
Hilaladiii <abdulhilal242@gmail.com>
2024-06-28 10:48:16 +0700

    feat : firebase function get spesific data & get limit data

--- src/common/db/blog.ts
+++ src/common/db/blog.ts
@@ -9,6 +9,11 @@ import {
   and,
   addDoc,
   updateDoc,
+  DocumentData,
+  deleteDoc,
+  orderBy,
+  limit,
+  FirestoreError,
 } from "firebase/firestore";
 import {
   ref,
@@ -25,7 +30,6 @@ const firestore = getFirestore(app);
 export async function createPost(data: BlogType) {
   try {
     const fileRef = ref(storage, `files/${data.image.name}`);
-    console.log(fileRef);
     const uploadTask = uploadBytesResumable(fileRef, data.image);
     await new Promise((resolve, reject) => {
       uploadTask.on(
@@ -40,12 +44,32 @@ export async function createPost(data: BlogType) {
       );
     });
     const fileUrl = await getDownloadURL(fileRef);
+    const date = new Date();
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const formattedDate = `${monthNames[date.getMonth()]} ${String(date.getDate()).padStart(2, "0")},${date.getFullYear()}`;
+
     if (fileUrl) {
       const blogData = {
         title: data.title,
         content: data.content,
         image: fileUrl,
         author: data.author,
+        category: data.category,
+        createdAt: formattedDate,
+        views: 0,
       };
       await addDoc(collection(firestore, "posts"), blogData);
       return {
@@ -89,17 +113,124 @@ export async function getData(collectionName: string) {
 
 export async function getDataById(collectionName: string, id: string) {
   try {
-    const snapshot = await getDoc(doc(firestore, collectionName, id));
+    const documentRef = doc(firestore, collectionName, id);
+    const snapshot = await getDoc(documentRef);
+
+    if (!snapshot.exists()) {
+      return {
+        status: 404,
+        message: `Document with id ${id} not found in ${collectionName} collection`,
+      };
+    }
+
     const data = snapshot.data();
+    const updatedData = {
+      ...data,
+      views: (data.views || 0) + 1,
+    };
+
+    await updateDoc(documentRef, { views: updatedData.views });
+
     return {
       status: 200,
-      message: `success get ${collectionName} by id`,
+      message: `Success getting ${collectionName} by id`,
+      data: updatedData,
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      message: (error as FirebaseError).message,
+    };
+  }
+}
+
+export async function getSpesificData({
+  collectionName,
+  field,
+  operator,
+  queryValue,
+}: {
+  collectionName: string;
+  field: string;
+  queryValue: string;
+  operator: "==" | "!=" | "<" | "<=" | ">" | ">=" | "in" | "not-in";
+}) {
+  try {
+    const q = query(
+      collection(firestore, collectionName),
+      where(field, operator, queryValue),
+    );
+    const querySnapshot = await getDocs(q);
+
+    const data = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      data: doc.data(),
+    }));
+    if (data.length <= 0) {
+      return {
+        status: 404,
+        data: [],
+        message: "your post is empty",
+      };
+    }
+    return {
+      status: 200,
       data: data,
     };
   } catch (error) {
     return {
       status: 500,
       message: (error as FirebaseError).message,
+    };
+  }
+}
+
+export async function getDataLimit({
+  collectionName,
+  limitValue,
+  orderByField,
+}: {
+  collectionName: string;
+  limitValue: number;
+  orderByField: string;
+}) {
+  try {
+    const q = await query(
+      collection(firestore, collectionName),
+      limit(limitValue),
+      orderBy(orderByField, "desc"),
+    );
+
+    const snapshoot = await getDocs(q);
+    const data = await snapshoot.docs.map((doc) => ({
+      id: doc.id,
+      data: doc.data(),
+    }));
+    return {
+      status: 200,
+      data: data,
+      message: "success get data",
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      message: (error as FirebaseError).message,
+    };
+  }
+}
+
+export async function deletePost(id: string) {
+  try {
+    const parsedId = JSON.parse(id).id;
+    await deleteDoc(doc(firestore, "posts", parsedId));
+    return {
+      status: 200,
+      message: "success delete post",
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      message: (error as FirestoreError).message,
     };
   }
 }
