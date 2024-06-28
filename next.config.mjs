afd72edc91e9df8bbccb9a3e5dc526f5c8089a1d
Hilaladiii <abdulhilal242@gmail.com>
2024-06-28 09:56:31 +0700

    feat : configure image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.FIREBASE_IMAGE_HOSTNAME,
+        pathname: process.env.FIREBASE_IMAGE_PATHNAME,
+      },
+    ],
+  },
+};
 
 export default nextConfig;
