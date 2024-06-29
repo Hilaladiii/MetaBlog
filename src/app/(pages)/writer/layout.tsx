8513bc20e9252d46569c0c4a1351edd0678b8683
Hilaladiii <abdulhilal242@gmail.com>
2024-06-29 07:58:33 +0700

    feat : add writer layout

---
+++ src/app/(pages)/writer/layout.tsx
@@ -0,0 +1,4 @@
+import WriterLayout from "@/common/components/layouts/WriterLayout";
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <WriterLayout>{children}</WriterLayout>;
+}
