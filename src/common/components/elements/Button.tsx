480597bfecdecde08e8c90dc8082b27725cac705
Hilaladiii <abdulhilal242@gmail.com>
2024-07-02 17:56:44 +0700

    chore : add size props

--- src/common/components/elements/Button.tsx
+++ src/common/components/elements/Button.tsx
@@ -3,7 +3,7 @@ import React, { ButtonHTMLAttributes } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "secondary" | "black" | "button1";
-  size?: "big" | "small";
+  size?: "big" | "small" | "full";
   className?: string;
   children: React.ReactNode;
 }
@@ -22,13 +22,15 @@ const Button = ({
         {
           "px-8 py-3": size === "big",
           "px-5 py-2": size === "small",
-          "bg-secondary2 text-white hover:bg-hover1": variant === "primary",
-          "border border-black/50 bg-white text-black hover:border-black/30 hover:bg-white/30 hover:text-text1":
+          "w-full px-5 py-2": size === "full",
+          "bg-secondary2 hover:bg-hover1 text-white": variant === "primary",
+          "hover:text-text1 border border-black/50 bg-white text-black hover:border-black/30 hover:bg-white/30":
             variant === "secondary",
-          "bg-black text-white hover:bg-black/80": variant === "black",
+          "bg-black text-white hover:bg-black/80 disabled:bg-black/40":
+            variant === "black",
           "bg-button1 text-text hover:bg-button1/80": variant == "button1",
         },
-        className
+        className,
       )}
     >
       {props.children}
