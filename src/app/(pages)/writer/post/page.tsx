ba73107f88bfcff86a2f9cacc07feb5ef18279b5
Hilaladiii <abdulhilal242@gmail.com>
2024-07-02 21:50:28 +0700

    refactor : dynamic import fix error

--- src/app/(pages)/writer/post/page.tsx
+++ src/app/(pages)/writer/post/page.tsx
@@ -1,5 +1,9 @@
-import BlogPost from "@/modules/writer-post";
+import dynamic from "next/dynamic";
+
+const BlogPostDynamic = dynamic(() => import("@/modules/writer-post"), {
+  ssr: false,
+});
 
 export default function WriterPost() {
-  return <BlogPost />;
+  return <BlogPostDynamic />;
 }
