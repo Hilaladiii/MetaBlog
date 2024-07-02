3677c62185a5d19b3f565feb0a2acac50e1d7a02
Hilaladiii <abdulhilal242@gmail.com>
2024-07-02 18:02:00 +0700

    chore : get top blog from api

--- src/modules/blog/module/TopBlog.tsx
+++ src/modules/blog/module/TopBlog.tsx
@@ -1,17 +1,31 @@
 import Badge from "@/common/components/elements/Badge";
 import Author from "@/common/components/fragments/Author";
+import { BlogType } from "@/common/types/blog";
+import { getTrendingPost } from "@/services/blog";
+import Image from "next/image";
 
-export default function TopBlog() {
+export default async function TopBlog() {
+  const data = await getTrendingPost(1);
+  const topBlog = data.data as { id: string; data: BlogType }[];
+
   return (
-    <div className="relative mx-auto h-[60vh] max-w-[68em] rounded-md bg-[url('/topblog.png')] bg-cover bg-center">
-      <div className="absolute bottom-3 left-3 max-w-xl p-5">
-        <Badge title="technology" />
+    <div className="relative mx-auto h-[60vh] max-w-[68em] overflow-hidden rounded-md">
+      <Image
+        src={topBlog[0].data.image}
+        alt="Top Blog Background"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="z-0"
+      />
+      <div className="absolute bottom-3 left-3 z-10 max-w-xl p-5">
+        <Badge title={topBlog[0].data.category} />
         <h1 className="mb-6 mt-4 text-2xl font-semibold text-white">
-          The Impact of Technology on the Workplace: How Technology is Changing
+          {topBlog[0].data.title}
         </h1>
         <Author
-          date="August 20, 2022"
-          name="Jason Fransisco"
+          date={topBlog[0].data.createdAt || ""}
+          name={topBlog[0].data.author}
           image=""
           variant="white"
         />
