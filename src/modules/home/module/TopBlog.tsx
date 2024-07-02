debae8b72e0a2bb9dc9c311b51d515bb814c88af
Hilaladiii <abdulhilal242@gmail.com>
2024-07-02 18:16:39 +0700

    chore : get 4 top posts from api

--- src/modules/home/module/TopBlog.tsx
+++ src/modules/home/module/TopBlog.tsx
@@ -1,32 +1,40 @@
 import Badge from "@/common/components/elements/Badge";
 import Author from "@/common/components/fragments/Author";
+import { BlogType } from "@/common/types/blog";
+import { getTrendingPost } from "@/services/blog";
 import Image from "next/image";
 
-export default function TopBlog() {
+export default async function TopBlog() {
+  const data = await getTrendingPost(4);
+  const blogData = data.data as { id: string; data: BlogType }[];
+  console.log(blogData[0].data);
   return (
     <div className="relative mx-auto grid h-[70vh] max-w-[68em] grid-cols-4 grid-rows-2 gap-5 rounded-md">
       <div className="relative col-span-2 row-span-2">
         <Image
-          src="/topblog1.jpeg"
-          alt="blog"
+          src={blogData[0].data.image}
+          alt={blogData[0].data.title}
           priority
           layout="fill"
           objectFit="cover"
           className="rounded-md"
         />
         <div className="absolute bottom-6 left-6">
-          <Badge title="Technology" />
+          <Badge title={blogData[0].data.category} />
           <h1 className="mb-5 mt-4 text-3xl font-semibold text-white">
-            The Impact of Technology on the Workplace: How Technology is
-            Changing
+            {blogData[0].data.title}
           </h1>
-          <Author date="August 20, 2022" name="Jason Fransisco" image="" />
+          <Author
+            date={blogData[0].data.createdAt || ""}
+            name={blogData[0].data.author}
+            image=""
+          />
         </div>
       </div>
       <div className="relative col-span-2 row-span-1">
         <Image
-          src="/topblog2.jpeg"
-          alt="blog"
+          src={blogData[1].data.image}
+          alt={blogData[1].data.title}
           priority
           layout="fill"
           objectFit="cover"
@@ -34,18 +42,21 @@ export default function TopBlog() {
           className="rounded-md"
         />
         <div className="absolute bottom-6 left-6">
-          <Badge title="Technology" />
+          <Badge title={blogData[1].data.category} />
           <h1 className="mb-5 mt-4 text-2xl font-semibold text-white">
-            The Impact of Technology on the Workplace: How Technology is
-            Changing
+            {blogData[1].data.title}
           </h1>
-          <Author date="August 20, 2022" name="Jason Fransisco" image="" />
+          <Author
+            date={blogData[1].data.createdAt || ""}
+            name={blogData[1].data.author}
+            image=""
+          />
         </div>
       </div>
       <div className="relative col-span-1 row-span-1">
         <Image
-          src="/topblog3.png"
-          alt="blog"
+          src={blogData[2].data.image}
+          alt={blogData[2].data.title}
           priority
           layout="fill"
           objectFit="cover"
@@ -53,17 +64,16 @@ export default function TopBlog() {
           className="rounded-md"
         />
         <div className="absolute bottom-0 left-3">
-          <Badge title="Technology" />
+          <Badge title={blogData[2].data.category} />
           <h1 className="mb-5 mt-4 text-xl font-semibold text-white">
-            The Impact of Technology on the Workplace: How Technology is
-            Changing
+            {blogData[2].data.title}
           </h1>
         </div>
       </div>
       <div className="relative col-span-1 row-span-1">
         <Image
-          src="/topblog4.jpeg"
-          alt="blog"
+          src={blogData[3].data.image}
+          alt={blogData[3].data.title}
           priority
           layout="fill"
           objectFit="cover"
@@ -71,10 +81,9 @@ export default function TopBlog() {
           className="rounded-md"
         />
         <div className="absolute bottom-0 left-3">
-          <Badge title="Technology" />
+          <Badge title={blogData[3].data.category} />
           <h1 className="mb-5 mt-4 text-xl font-semibold text-white">
-            The Impact of Technology on the Workplace: How Technology is
-            Changing
+            {blogData[3].data.title}
           </h1>
         </div>
       </div>
