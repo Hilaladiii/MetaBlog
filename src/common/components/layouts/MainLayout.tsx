f5fbfe8e9ad9e0c9776fc0e1b4fe70305d8d095f
Hilaladiii <abdulhilal242@gmail.com>
2024-07-02 18:01:42 +0700

    chore : configure dark mode

--- src/common/components/layouts/MainLayout.tsx
+++ src/common/components/layouts/MainLayout.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from "@/common/hooks/useTheme";
 import Footer from "../fragments/Footer";
 import Navbar from "../fragments/Navbar";
 
@@ -6,10 +7,12 @@ export default function MainLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const theme = useTheme();
   return (
-    <div>
-      <Navbar />
-      <div className="mt-36">
+    <div className="dark:bg-dark2">
+      <Navbar initialTheme={theme} />
+      <div className="dark:bg-dark2 h-32 w-full" />
+      <div>
         <div className="mx-auto max-w-[68em]">{children}</div>
       </div>
       <Footer />
