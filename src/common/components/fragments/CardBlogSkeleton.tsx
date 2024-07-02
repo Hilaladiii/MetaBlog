6c6803f3e207ff7a774742756ca9289e2e84e972
Hilaladiii <abdulhilal242@gmail.com>
2024-07-02 17:59:22 +0700

    feat : add card blog skeleton

---
+++ src/common/components/fragments/CardBlogSkeleton.tsx
@@ -0,0 +1,10 @@
+export default function CardBlogSkeleton() {
+  return (
+    <div className="max-w-sm animate-pulse rounded-md border-2 border-cloud bg-gray-100 p-3">
+      <div className="mb-6 h-[250px] w-full rounded-md bg-gray-200"></div>
+      <div className="h-5 w-32 bg-gray-200"></div>
+      <div className="my-4 mb-6 h-24 w-full bg-gray-200"></div>
+      <div className="h-8 w-3/4 bg-gray-200"></div>
+    </div>
+  );
+}
