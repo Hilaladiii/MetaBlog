3933e1419eefef47f2737426cdee2e5346916583
Hilaladiii <abdulhilal242@gmail.com>
2024-07-04 21:51:14 +0700

    feat : responsive blog page mobile device

--- src/modules/blog/module/Blog.tsx
+++ src/modules/blog/module/Blog.tsx
@@ -7,23 +7,21 @@ export default async function Blog() {
   const data = await getPosts();
   const blogData = data.data as { id: string; data: BlogType }[];
   return (
-    <div>
-      <div className="w-full">
-        <TopBlog />
-        <div className="mt-20 grid grid-cols-3 gap-5">
-          {blogData &&
-            blogData.map((blog, index) => (
-              <CardBlog
-                title={blog.data.title}
-                author={blog.data.author}
-                image={blog.data.image}
-                date={blog.data.createdAt || ""}
-                category={blog.data.category}
-                href={`/blog/${blog.id}`}
-                key={index}
-              />
-            ))}
-        </div>
+    <div className="w-full">
+      <TopBlog />
+      <div className="mt-20 grid grid-cols-1 gap-5 md:grid-cols-3">
+        {blogData &&
+          blogData.map((blog, index) => (
+            <CardBlog
+              title={blog.data.title}
+              author={blog.data.author}
+              image={blog.data.image}
+              date={blog.data.createdAt || ""}
+              category={blog.data.category}
+              href={`/blog/${blog.id}`}
+              key={index}
+            />
+          ))}
       </div>
     </div>
   );
